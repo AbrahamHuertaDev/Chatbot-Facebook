32de40e938b009aea56e2685820475a218810a22
emerdeveloper <emersonjavid97@gmail.com>
2017-10-15 16:12:39 -0500

    it's working full, upload on Bluemix

--- controllers/verification.js
+++ controllers/verification.js
@@ -4,7 +4,7 @@ module.exports = (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
 
     const hubMode = req.query['hub.mode'];
-    const verifyTokenMatches = (req.query['hub.verify_token'] === 'botcube is cool');
+    const verifyTokenMatches = (req.query['hub.verify_token'] === 'my_token_que_verifica_en_facebook');//este es el token que se configura en developers.facebook
 
     if (hubMode && verifyTokenMatches) {
         res.status(200).send(hubChallenge);
